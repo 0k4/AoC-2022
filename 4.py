464c3419f685064938103eaeb691acaef13bd2da
0k4 <bono.vanderpoorten@gmail.com>
2022-12-05 01:25:34 +0100

    pure horror

---
+++ 4.py
@@ -0,0 +1,14 @@
+with open('./input/4.txt') as f:
+    c = 0
+    def zitLijstinLijst(A, B):
+        print(', '.join(map(str, A))+'-----------'+', '.join(map(str, B)))
+        return ', '.join(map(str, A)) in ', '.join(map(str, B))
+    for elfenpaar in f.read().split("\n"):
+        g1 = int(elfenpaar.split(sep=',')[0].split('-')[0])
+        g2 = int(elfenpaar.split(sep=',')[0].split('-')[1])
+        g3 = int(elfenpaar.split(sep=',')[1].split('-')[0])
+        g4 = int(elfenpaar.split(sep=',')[1].split('-')[1])
+        l,l2 = [i for i in range(g1,g2+1)],[i for i in range(g3,g4+1)]
+        if zitLijstinLijst(l,l2) or zitLijstinLijst(l2,l):
+            c += 1
+    print(c)
