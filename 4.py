3de8682b5bf517eab8e6d62f36afd30f8412d6e8
0k4 <bono.vanderpoorten@gmail.com>
2022-12-05 02:15:36 +0100

    \#

--- 4.py
+++ 4.py
@@ -4,7 +4,7 @@ with open('./input/4.txt') as f:
     for elfenpaar in f.read().split("\n"):
         g1,g2,g3,g4 = int(elfenpaar.split(sep=',')[0].split('-')[0]),int(elfenpaar.split(sep=',')[0].split('-')[1]),int(elfenpaar.split(sep=',')[1].split('-')[0]),int(elfenpaar.split(sep=',')[1].split('-')[1])
         l,l2 = [i for i in range(g1,g2+1)],[i for i in range(g3,g4+1)]
-        if ', '+', '.join(map(str, l))+',' in ', '+', '.join(map(str, l2))+',' or ', '+', '.join(map(str, l2))+',' in ', '+', '.join(map(str, l))+',':
+        if '#'+'#'.join(map(str, l))+'#' in '#'+'#'.join(map(str, l2))+'#' or '#'+'#'.join(map(str, l2))+'#' in '#'+'#'.join(map(str, l))+'#':
             c += 1
         #PT 2
         for nummer in l:
