44ad0e1bbf1b872094f3c7cde5c1a2804669f68f
0k4 <bono.vanderpoorten@gmail.com>
2022-12-05 01:53:35 +0100

    Pog

--- 4.py
+++ 4.py
@@ -1,13 +1,8 @@
 with open('./input/4.txt') as f:
     c = 0
-    def zitLijstinLijst(A, B):
-        return ', '.join(map(str, A)) in ', '.join(map(str, B))
     for elfenpaar in f.read().split("\n"):
-        g1 = int(elfenpaar.split(sep=',')[0].split('-')[0])
-        g2 = int(elfenpaar.split(sep=',')[0].split('-')[1])
-        g3 = int(elfenpaar.split(sep=',')[1].split('-')[0])
-        g4 = int(elfenpaar.split(sep=',')[1].split('-')[1])
+        g1,g2,g3,g4 = int(elfenpaar.split(sep=',')[0].split('-')[0]),int(elfenpaar.split(sep=',')[0].split('-')[1]),int(elfenpaar.split(sep=',')[1].split('-')[0]),int(elfenpaar.split(sep=',')[1].split('-')[1])
         l,l2 = [i for i in range(g1,g2+1)],[i for i in range(g3,g4+1)]
-        if zitLijstinLijst(l,l2) or zitLijstinLijst(l2,l):
+        if ', '+', '.join(map(str, l))+',' in ', '+', '.join(map(str, l2))+',' or ', '+', '.join(map(str, l2))+',' in ', '+', '.join(map(str, l))+',':
             c += 1
     print(c)
